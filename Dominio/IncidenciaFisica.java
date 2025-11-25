955206badb2b2a1fe99d4e26758ead6c61c6fe62
Doraemon77777 <105044anderson@gmail.com>
2025-11-25 13:51:09 -0500

    Add files via upload

---
+++ Dominio/IncidenciaFisica.java
@@ -0,0 +1,36 @@
+package Dominio;
+
+public class IncidenciaFisica extends Incidencia {
+    private String idIncidenciaFisica;
+    private String ubicacion;
+    private String descripcionFisica;
+
+    public IncidenciaFisica() { super(); }
+
+    public IncidenciaFisica(String idIncidencia, String idUsuario, String tipoIncidencia,String descripcion, String fechaRegistro, String estado, String referencia, String prioridad, String idIncidenciaFisica, String ubicacion, String descripcionFisica) {
+        super(idIncidencia, idUsuario, tipoIncidencia, descripcion, fechaRegistro, estado, referencia, prioridad);
+        this.idIncidenciaFisica = idIncidenciaFisica;
+        this.ubicacion = ubicacion;
+        this.descripcionFisica = descripcionFisica;
+    }
+    public String getIdIncidenciaFisica() {
+        return idIncidenciaFisica; }
+    public void setIdIncidenciaFisica(String idIncidenciaFisica) {
+        this.idIncidenciaFisica = idIncidenciaFisica; }
+    public String getUbicacion() {
+        return ubicacion;
+    }
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+    public String getDescripcionFisica() {
+        return descripcionFisica;
+    }
+    public void setDescripcionFisica(String descripcionFisica) {
+        this.descripcionFisica = descripcionFisica;
+    }
+    public String toString() {
+        return "IncidenciaFisica: idIncidenciaFisica=" + idIncidenciaFisica + ", ubicacion= " + ubicacion +
+                ", descripcionFisica=" + descripcionFisica + ", base=" + super.toString() + "";
+    }
+}
